fd785829af818ab8656b31f1f36404ba7a40843e
Teddy Engel <engel.teddy@gmail.com>
2015-02-23 14:57:01 +0000

    - Heapsort implementation

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Teddy Engel. All rights reserved.
 //
 
+#include <string>
+#include <vector>
 #include <iostream>
 
 void PrintVector(std::vector<int>& values)
@@ -18,14 +20,95 @@ void PrintVector(std::vector<int>& values)
     }
 }
 
+/*
+** Manipulation related
+*/
+void Swap(std::vector<int>& values, const int& n1, const int& n2)
+{
+    int nTmp = values[n1];
+    values[n1] = values[n2];
+    values[n2] = nTmp;
+}
+
+/*
+** Binary tree related
+*/
+int Parent(const int& position)
+{
+    return ((position + 1) / 2) - 1;
+}
+
+int Left(const int& position)
+{
+    return ((position + 1) * 2) - 1;
+}
+
+int Right(const int& position)
+{
+     return ((2 * (position + 1)) + 1) - 1;
+}
+
+/*
+** Heap related
+*/
+void Heapify(std::vector<int>& heap, const int& nIndex, const int& nHeapSize)
+{
+    int nLeft = Left(nIndex);
+    int nRight = Right(nIndex);
+    int nLargest = -1;
+
+    if (nLeft < nHeapSize && heap[nLeft] > heap[nIndex])
+        nLargest = nLeft;
+    else
+        nLargest = nIndex;
+    if (nRight < nHeapSize && heap[nRight] > heap[nLargest])
+        nLargest = nRight;
+    if (nLargest != nIndex)
+    {
+        // Swap both
+        Swap(heap, nIndex, nLargest);
+        // Recursively call heapify
+        Heapify(heap, nLargest, nHeapSize);
+    }
+
+}
+
+void BuildHeap(std::vector<int>& heap)
+{
+    int nHeapSize = heap.size();
+
+    for (int i = (nHeapSize / 2) - 1; i > -1; i--)
+    {
+        Heapify(heap, i, nHeapSize);
+    }
+}
+
+void HeapSort(std::vector<int>& values)
+{
+    // Getting the heap size
+    int nHeapSize = values.size();
+
+    // We build the heap from the collection of values
+    BuildHeap(values);
+
+    // Then we sort the heap
+    for (int i = values.size() - 1; i > 0; i--)
+    {
+        Swap(values, 0, i);
+        nHeapSize = nHeapSize - 1;
+        Heapify(values, 0, nHeapSize);
+    }
+}
+
 int main(int argc, const char * argv[]) {
     std::vector<int> values = {10, 0, 2, 5, -4, 3, 2};
-    std::vector<int> result;
     
     std::cout << "Unsorted collection:" << std::endl;
     PrintVector(values);
-    MergeSort(values, result);
+    HeapSort(values);
     std::cout << "Sorted collection:" << std::endl;
-    PrintVector(result);
+    PrintVector(values);
+
+    values.clear();
     return 0;
 }
